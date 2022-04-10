a60c8b26ab242044cc5fe3cee698a8e5eb15fad3
rezuwii <59052194+rezuwii@users.noreply.github.com>
2022-04-10 12:27:23 +0530

    changed token

--- main.py
+++ main.py
@@ -50,6 +50,7 @@ async def main():
         print("Cogs Loadd\nBot ready!")
         # await bot.start('OTQ5ODUyMDM3MzIxOTkwMTY2.YiQYpQ.24uOmgwVCWjs5Z4lYzx5Rk3Z4ac')
         # changed token
+        # yes
         await bot.start('OTYyNjAzODQ2Njk2MzM3NDA4.YlJ8sQ.VkZ-uNKB5Z1QCxbguCufUe3wth4')
 
 
