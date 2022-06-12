1dd831379c2cdfdba89ac47497ff1d85b1ad0ca2
rezuwii <59052194+rezuwii@users.noreply.github.com>
2022-06-12 22:01:11 +0530

    test remove

--- main.py
+++ main.py
@@ -340,10 +340,6 @@ async def on_raw_reaction_add(payload):
                 #role_b = discord.utils.get(user.guild.roles, name="Blocked")
                 server = bot.get_guild(94355643464328498)
                 msg_owner = server.get_member(int(db_data["author_id"]))
-                if server.get_member(msg_owner) is None: 
-                    user_a.send("Vent message owner not found! They probably left the server.")
-                else:
-                    pass
                 # print(msg_owner)
                 #print(f"msg_owner: {msg_owner}")
                 #print(f"user_a: {user_a}")
