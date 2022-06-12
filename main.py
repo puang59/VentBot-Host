a4bdcd747284057a7897f67d5dd997ed2afbe19c
rezuwii <59052194+rezuwii@users.noreply.github.com>
2022-06-12 16:15:53 +0530

    added header

--- main.py
+++ main.py
@@ -156,6 +156,7 @@ async def on_message(msg):
                                     global accept
 
                                     async def accept():
+                                        em.set_author(name="Anonymous", icon_url="https://res.cloudinary.com/teepublic/image/private/s--UymRXkch--/t_Resized%20Artwork/c_fit,g_north_west,h_1054,w_1054/co_ffffff,e_outline:53/co_ffffff,e_outline:inner_fill:53/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_auto,h_630,q_90,w_630/v1570281377/production/designs/6215195_0.jpg")
                                         em.set_footer(
                                             text="You can click on speech-bubble emoji to reply to this vent and talk to the author anonymously.", icon_url="https://kidsattennis.ca/wp-content/uploads/2020/05/greenball.png")
                                         x = await vent_channel.send(embed=em)
