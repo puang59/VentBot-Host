9bcb73a1fb03c48d54bc54f58511ba037b07d342
rezuwii <59052194+rezuwii@users.noreply.github.com>
2022-06-30 23:22:29 +0530

    added angular

--- main.py
+++ main.py
@@ -414,7 +414,7 @@ async def on_message(msg):
                                     tagData = vCheck.find_one({"user": msg.author.id})
 
                                     em = discord.Embed(
-                                        description=f"{tagData['tags']}\n\n{msg.content}"
+                                        description=f"> {tagData['tags']}\n\n{msg.content}"
                                     )
 
                                     em.set_author(name="Anonymous", icon_url="https://res.cloudinary.com/teepublic/image/private/s--UymRXkch--/t_Resized%20Artwork/c_fit,g_north_west,h_1054,w_1054/co_ffffff,e_outline:53/co_ffffff,e_outline:inner_fill:53/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_auto,h_630,q_90,w_630/v1570281377/production/designs/6215195_0.jpg")
