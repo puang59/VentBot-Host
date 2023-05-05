0a01f02ee73fe47c27b3bff4d45c1779dad3bc85
rezuwii <59052194+rezuwii@users.noreply.github.com>
2023-05-05 15:28:24 +0530

    globalwarn channel issue

--- _ventV2.0/cogs/_utility.py
+++ _ventV2.0/cogs/_utility.py
@@ -280,17 +280,16 @@ class _utility(commands.Cog):
 
     @commands.command()
     @commands.check(lambda ctx: ctx.author.id in admins)
-    async def globalwarn(self, ctx, channel, *, message = None):
+    async def globalwarn(self, ctx, channel: discord.TextChannel, *, message = None):
         if message == None: 
             await ctx.send("Please provide a valid message")
         else: 
-            chn = self.bot.get_channel(channel.id)
             em = discord.Embed(
                 description=f"{message.content}", 
                 color=discord.Colour.red()
             )
             em.set_author(name="Moderator", icon_url="https://res.cloudinary.com/teepublic/image/private/s--UymRXkch--/t_Resized%20Artwork/c_fit,g_north_west,h_1054,w_1054/co_ffffff,e_outline:53/co_ffffff,e_outline:inner_fill:53/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_auto,h_630,q_90,w_630/v1570281377/production/designs/6215195_0.jpg")
-            await chn.send(embed = em)
+            await channel.send(embed = em)
 
 async def setup(bot):
     await bot.add_cog(_utility(bot))
