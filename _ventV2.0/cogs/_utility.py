a9dd9cedc6082a2f69a45a9851317ba04263f701
rezuwii <59052194+rezuwii@users.noreply.github.com>
2023-05-05 15:29:12 +0530

    message.content fix

--- _ventV2.0/cogs/_utility.py
+++ _ventV2.0/cogs/_utility.py
@@ -285,7 +285,7 @@ class _utility(commands.Cog):
             await ctx.send("Please provide a valid message")
         else: 
             em = discord.Embed(
-                description=f"{message.content}", 
+                description=f"{message}", 
                 color=discord.Colour.red()
             )
             em.set_author(name="Moderator", icon_url="https://res.cloudinary.com/teepublic/image/private/s--UymRXkch--/t_Resized%20Artwork/c_fit,g_north_west,h_1054,w_1054/co_ffffff,e_outline:53/co_ffffff,e_outline:inner_fill:53/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_auto,h_630,q_90,w_630/v1570281377/production/designs/6215195_0.jpg")
